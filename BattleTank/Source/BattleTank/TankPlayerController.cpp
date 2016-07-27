d91efe7f076cb6efdcd04c8240232868f40f85bf
James Kingsley <jameslkingsley@outlook.com>
2016-07-27 03:38:45 +0100

    Implement crosshair world vector methods

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -13,6 +13,65 @@ void ATankPlayerController::BeginPlay() {
     }
 }
 
+void ATankPlayerController::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
     return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() const {
+    if (!GetControlledTank()) { return; }
+
+    FVector HitLocation;
+    if (GetSightRayHitLocation(HitLocation)) {
+        UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), *(HitLocation.ToString()));
+    }
+
+    return;
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const {
+    int32 ViewPortSizeX, ViewPortSizeY;
+    GetViewportSize(ViewPortSizeX, ViewPortSizeY);
+
+    auto ScreenLocation = FVector2D(ViewPortSizeX * CrosshairXLocation, ViewPortSizeY * CrosshairYLocation);
+
+    FVector LookDirection;
+    if (GetLookDirection(ScreenLocation, LookDirection)) {
+        return GetLookVectorHitLocation(LookDirection, HitLocation);
+    }
+
+    return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const {
+    FVector CameraWorldLocation;
+    return DeprojectScreenPositionToWorld(
+        ScreenLocation.X,
+        ScreenLocation.Y,
+        CameraWorldLocation,
+        LookDirection
+    );
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const {
+    FHitResult HitResult;
+    auto CameraLocation = PlayerCameraManager->GetCameraLocation();
+    auto TargetLocation = CameraLocation + (LookDirection * LineTraceRange);
+
+    if (GetWorld()->LineTraceSingleByChannel(
+        HitResult,
+        CameraLocation,
+        TargetLocation,
+        ECollisionChannel::ECC_Visibility
+    )) {
+        HitLocation = HitResult.Location;
+        return true;
+    }
+
+    HitLocation = FVector(0.f);
+    return false;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -8,7 +8,22 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController {
     GENERATED_BODY()
 
-public:
+private:
+    UPROPERTY(EditAnywhere)
+    float CrosshairXLocation = 0.5f;
+
+    UPROPERTY(EditAnywhere)
+    float CrosshairYLocation = 0.3333f;
+
+    UPROPERTY(EditAnywhere)
+    float LineTraceRange = 100000.0f;
+
     virtual void BeginPlay() override;
+    virtual void Tick(float) override;
+
     ATank* GetControlledTank() const;
+    void AimTowardsCrosshair() const;
+    bool GetSightRayHitLocation(FVector&) const;
+    bool GetLookDirection(FVector2D, FVector&) const;
+    bool GetLookVectorHitLocation(FVector, FVector&) const;
 };
